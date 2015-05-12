0b2a28653073bbdf1233e16a438f8bcbda51804c
tormibg <todord@gmail.com>
2015-05-12 17:26:22 +0300

    03.MultidimensionalArrays

---
+++ Homework/Advanced C#/03.MultidimensionalArrays/03.MultidimensionalArrays/01.FillTheMatrix/01.FillTheMatrix.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+internal class FillTheMatrix
+{
+    static void Main()
+    {
+        int size = int.Parse(Console.ReadLine());
+        MatrixPatternA(size);
+
+        Console.WriteLine();
+
+        MatrixPatternB(size);
+    }
+
+    static void MatrixPatternA(int n)
+    {
+        int counter = 1;
+        int[,] matAInts = new int[n, n];
+        for (int col = 0; col < n; col++)
+        {
+            for (int row = 0; row < n; row++)
+            {
+                matAInts[row, col] = counter;
+                counter++;
+            }
+        }
+        PrintMatrix(matAInts);
+    }
+    static void MatrixPatternB(int n)
+    {
+        int[,] matBInts = new int[n, n];
+        int counter = 1;
+        for (int col = 0; col < n; col++)
+        {
+            if (col % 2 == 0)
+            {
+                for (int row = 0; row < n; row++)
+                {
+                    matBInts[row, col] = counter;
+                    counter++;
+                }
+            }
+            else
+            {
+                for (int row = n - 1; row >= 0; row--)
+                {
+                    matBInts[row, col] = counter;
+                    counter++;
+                }
+            }
+        }
+        PrintMatrix(matBInts);
+    }
+    static void PrintMatrix(int[,] mat)
+    {
+        for (int i = 0; i < mat.GetLength(0); i++)
+        {
+            for (int j = 0; j < mat.GetLength(1); j++)
+            {
+                Console.Write("{0,3}", mat[i, j]);
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Homework/Advanced C#/03.MultidimensionalArrays/03.MultidimensionalArrays/02.MaximalSum/MaximalSum.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Linq;
+
+class MaximalSum
+{
+    static void Main()
+    {
+        string[] str = Console.ReadLine().Split();
+        int iRow = int.Parse(str[0]);
+        int iCol = int.Parse(str[1]);
+
+        int newRows = 3;
+        int newCols = 3;
+        int[,] matrix = new int[iRow + 1, iCol + 1];
+
+        for (int i = 0; i < iRow; i++)
+        {
+            int[] numbers =
+                Console.ReadLine()
+                    .Trim()
+                    .Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(s => int.Parse(s))
+                    .ToArray();
+            for (int l = 0; l < iCol; l++)
+            {
+                matrix[i, l] = numbers[l];
+            }
+        }
+
+        int maxSum = 0;
+        int startRow = 0;
+        int startCol = 0;
+        for (int row = 0; row < matrix.GetLength(0) - (newRows - 1); row++)
+        {
+            for (int col = 0; col < matrix.GetLength(1) - (newCols - 1); col++)
+            {
+                int sum = 0;
+                for (int tempRow = row; tempRow < row + newRows; tempRow++)
+                {
+                    for (int tempCol = col; tempCol < col + newCols; tempCol++)
+                    {
+                        sum += matrix[tempRow, tempCol];
+                    }
+                }
+                if (sum > maxSum)
+                {
+                    maxSum = sum;
+                    startRow = row;
+                    startCol = col;
+                }
+            }
+        }
+
+        Console.WriteLine("Sum = {0}", maxSum);
+
+        for (int row = startRow; row < startRow + newRows; row++)
+        {
+            for (int col = startCol; col < startCol + newCols; col++)
+            {
+                Console.Write("{0,-4}", matrix[row, col]);
+            }
+            Console.WriteLine();
+        }
+        
+    }
+}
