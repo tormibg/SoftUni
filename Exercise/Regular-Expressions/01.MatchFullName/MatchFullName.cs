e8113630fcfc1ea00478358c1e0674fe32c128ac
tormibg <todord@gmail.com>
2015-05-18 16:36:22 +0300

    Exercise.Regular-Expressions

---
+++ Exercise/Regular-Expressions/01.MatchFullName/MatchFullName.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class MatchFullName
+{
+    static void Main()
+    {
+        string pattern = @"\b([A-Z][a-z]+) ([A-Z][a-z]+)\b";
+        string inputStr = Console.ReadLine();
+        Regex regex = new Regex(pattern);
+        Match mm = regex.Match(inputStr);
+        MatchCollection match = regex.Matches(inputStr);
+        Console.WriteLine(regex.Replace(inputStr, "Petar Petrov"));
+    }
+}
---
+++ Exercise/Regular-Expressions/02.MatchPhoneNumber/MatchPhoneNumber.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class MatchPhoneNumber
+{
+    static void Main()
+    {
+        string pattern = @"(\s*|\A)\+\d{3}(?<del> |\-)(?<dig>\d)\k<del>\d{3}\k<del>\d{4}\b";
+        Regex regiRegex = new Regex(pattern);
+        string inputStr =
+            "+359 2 222 2222, +359-2-222-2222, 359-2-222-2222, +359/2/222/2222, +359-2 222 2222,+359 2-222-2222, +359-2-222-222, +359-2-222-22222";
+        Match match = regiRegex.Match(inputStr);
+        MatchCollection mm = regiRegex.Matches(inputStr);
+        foreach (Match mmMatch in mm)
+        {
+            Console.WriteLine(mmMatch.Groups[0]);
+            Console.WriteLine(mmMatch.Groups["del"]);
+            Console.WriteLine(mmMatch.Groups["dig"]);
+        }
+    }
+}
