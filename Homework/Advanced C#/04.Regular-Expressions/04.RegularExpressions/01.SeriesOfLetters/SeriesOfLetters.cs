1c11438a1bf98d9aeafb841d32e899b82ba035f6
tormibg <todord@gmail.com>
2015-05-18 22:57:38 +0300

    04.Rgular

---
+++ Homework/Advanced C#/04.Regular-Expressions/04.RegularExpressions/01.SeriesOfLetters/SeriesOfLetters.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class SeriesOfLetters
+{
+    static void Main()
+    {
+        string inputStr = Console.ReadLine();
+        string pattern = @"(\w)\1+";
+        Console.WriteLine(Regex.Replace(inputStr, pattern, "$1"));
+    }
+}
