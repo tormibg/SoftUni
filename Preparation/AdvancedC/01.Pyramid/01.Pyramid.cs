74dd8799ee809393f372975b4461e7a35fbb2948
tormibg <todord@gmail.com>
2015-05-28 15:20:49 +0300

    Advanced C# Preparation

---
+++ Preparation/AdvancedC/01.Pyramid/01.Pyramid.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Pyramid
+{
+    static void Main()
+    {
+        int lineNumber = int.Parse(Console.ReadLine().Trim());
+        int prevNum = int.Parse(Console.ReadLine().Trim());
+        List<int> pyrNimList = new List<int>();
+        pyrNimList.Add(prevNum);
+        for (int i = 0; i < lineNumber-1; i++)
+        {
+            int[] numInts =
+                Console.ReadLine()
+                    .Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(x => int.Parse(x))
+                    .ToArray();
+
+            Array.Sort(numInts);
+            bool isGreat = false;
+            for (int j = 0; j < numInts.Length; j++)
+            {
+                if (numInts[j] > prevNum)
+                {
+                    prevNum = numInts[j];
+                    pyrNimList.Add(numInts[j]);
+                    isGreat = true;
+                    break;
+                }
+            }
+            if (!isGreat)
+            {
+                prevNum++;
+            }
+        }
+        Console.WriteLine(string.Join(", ",pyrNimList));
+    }
+}
---
+++ Preparation/AdvancedC/08.TextGravity/08.TextGravity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class TextGravity
+{
+    static void Main()
+    {
+        int rows = int.Parse(Console.ReadLine());
+        string strForMat = Console.ReadLine();
+        int cols = strForMat.Length/rows;
+        if (strForMat.Length % rows == 1)
+        {
+            cols++;
+        }
+    }
+}
