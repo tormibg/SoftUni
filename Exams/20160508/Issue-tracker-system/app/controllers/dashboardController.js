2127ee7bfc696fb71ff5e8ff27053c7c6cf2a067
tormibg <todord@gmail.com>
2016-05-07 16:28:23 +0300

    myIssue
    myIssue

--- Exams/20160508/Issue-tracker-system/app/controllers/dashboardController.js
+++ Exams/20160508/Issue-tracker-system/app/controllers/dashboardController.js
@@ -1,25 +1,53 @@
 "use strict";
 
-angular.module('issueTracker.controllers.DashboardController', ['issueTracker.services.identity', 'issueTracker.services.projects'])
+angular.module('issueTracker.controllers.DashboardController', ['issueTracker.services.identity', 'issueTracker.services.projects', 'issueTracker.services.issue'])
     .controller('DashboardController', [
         '$scope',
         'identity',
         'projects',
-        function ($scope, identity, projects) {
+        'issue',
+        function ($scope, identity, projects, issue) {
 
             $scope.prjParams = {
                 'pageNumber': 1,
                 'pageSize': 5
             };
 
-            reloadProject();
+            $scope.issParams = {
+                'pageNumber': 1,
+                'pageSize': 5
+            };
+
+            $scope.reloadDashboard = function () {
+                reloadProject();
+                reloadIssue();
+            };
+
+            $scope.reloadProject = function(){
+                reloadProject();
+            };
+
+            $scope.reloadIssue = function(){
+                reloadIssue();
+            };
+
+            $scope.reloadDashboard();
 
             function reloadProject() {
                 var userId = identity.getCurrentUser();
                 projects.getProjects($scope.prjParams, userId).then(
-                    function success(data){
+                    function success(data) {
                         $scope.myProjetcs = data;
                         /*console.log($scope.myProjetcs)*/
+                    }
+                )
+            }
+
+            function reloadIssue() {
+                issue.getMyIssue($scope.issParams).then(
+                    function success(data) {
+                        $scope.myIssues = data;
+                        /*console.log(data);*/
                     }
                 )
             }
---
+++ Exams/20160508/Issue-tracker-system/app/services/issue.js
@@ -0,0 +1,32 @@
+'use strict';
+
+angular.module('issueTracker.services.issue', [])
+    .factory('issue', [
+        '$http',
+        '$q',
+        'BASE_URL',
+        function ($http, $q, BASE_URL) {
+
+            function getMyIssue(params) {
+                var deferred = $q.defer();
+
+                var requestData = {
+                    method: 'GET',
+                    url: BASE_URL + 'issues/me?orderBy=DueDate desc' + '&pageSize=' + params.pageSize + '&pageNumber=' + params.pageNumber
+                };
+
+
+                $http(requestData)
+                    .then(function success(response) {
+                        deferred.resolve(response.data);
+                    });
+
+                return deferred.promise;
+
+            }
+
+            return {
+                getMyIssue: getMyIssue
+            }
+        }]);
+
