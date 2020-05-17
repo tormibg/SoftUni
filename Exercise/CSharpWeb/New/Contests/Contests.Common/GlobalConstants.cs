43357fa1278d1179cae4fef00a58354347aa45f2
todor <todord@gmail.com>
2020-05-17 15:50:14 +0300

    Contests start

---
+++ Exercise/CSharpWeb/New/Contests/Contests.Common/GlobalConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace Contests.Common
+{
+    public static class GlobalConstants
+    {
+        public const string SystemName = "Contests";
+
+        public const string AdministratorRoleName = "Administrator";
+    }
+}
---
+++ Exercise/CSharpWeb/New/Contests/Web/Contests.Web/Areas/Administration/Controllers/AdministrationController.cs
@@ -0,0 +1,14 @@
+﻿namespace Contests.Web.Areas.Administration.Controllers
+{
+    using Contests.Common;
+    using Contests.Web.Controllers;
+
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Authorize(Roles = GlobalConstants.AdministratorRoleName)]
+    [Area("Administration")]
+    public class AdministrationController : BaseController
+    {
+    }
+}
